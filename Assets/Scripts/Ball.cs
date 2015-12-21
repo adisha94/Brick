feeb6c8b42e1d8bfe8a8272c2f7d758f9e32a8c4
ADISH BETAWAR <abetawar@gmail.com>
2015-12-21 12:12:08 -0800

    initial commit

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour
+{
+	
+	public float ballInitialVelocity = 600f;
+	
+	
+	private Rigidbody rb;
+	private bool ballInPlay;
+
+
+	// upon wake up 
+	void Awake ()
+	{
+		
+		rb = GetComponent<Rigidbody>();
+		
+	}
+	
+	void Update () 
+	{
+		if (Input.GetButtonDown("Fire1") && ballInPlay == false) // ball has not been launched
+		{
+			transform.parent = null;
+			ballInPlay = true;
+			rb.isKinematic = false;
+			rb.AddForce(new Vector3(ballInitialVelocity, ballInitialVelocity, 0));
+		}
+	}
+}
