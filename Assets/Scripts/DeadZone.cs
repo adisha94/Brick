1c6e3f279023361635c5d9ba571b25984443c7b6
ADISH BETAWAR <abetawar@gmail.com>
2015-12-21 14:05:20 -0800

    updates to deadline script

--- Assets/Scripts/DeadZone.cs
+++ Assets/Scripts/DeadZone.cs
@@ -9,12 +9,14 @@ public class DeadZone : MonoBehaviour
 {
 
 	// Use this for initialization
-	void Start (){
+	void Start ()
+	{
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 	
 	}
 }
