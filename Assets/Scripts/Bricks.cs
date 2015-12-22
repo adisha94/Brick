0e064d89d6f25c7d0d2c5f6336893569e714c24b
ADISH BETAWAR <abetawar@gmail.com>
2015-12-21 16:42:34 -0800

    new things

--- Assets/Scripts/Bricks.cs
+++ Assets/Scripts/Bricks.cs
@@ -4,4 +4,11 @@ using System.Collections;
 public class Bricks : MonoBehaviour
 {
 	public GameObject brickParticle; // refers to the BrickParticle object in the scene view
+
+	void OnCollisionEnter(Collision other)
+	{
+		Instantiate (brickParticle, transform.position, Quaternion.identity);
+		GM.instance.DestroyBrick();
+		Destroy(gameObject);
+	}
 }
