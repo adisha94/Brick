a98d84c224afe97de8c41da23e51857bfb0469de
ADISH BETAWAR <abetawar@gmail.com>
2015-12-21 16:48:30 -0800

    new things
    particle engine working

--- Assets/Scripts/Bricks.cs
+++ Assets/Scripts/Bricks.cs
@@ -5,10 +5,11 @@ public class Bricks : MonoBehaviour
 {
 	public GameObject brickParticle; // refers to the BrickParticle object in the scene view
 
-	void OnCollisionEnter(Collision other)
+	void OnCollisionEnter()
 	{
 		Instantiate (brickParticle, transform.position, Quaternion.identity);
 		GM.instance.DestroyBrick();
 		Destroy(gameObject);
 	}
+
 }
